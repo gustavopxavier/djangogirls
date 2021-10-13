2e6d019ccf95b637557b2fd558ec4e4a5d523c07
gustavopxavier <gustavopxavier@gmail.com>
2021-10-13 10:14:47 -0300

    Dashboard

--- blog_panel/urls.py
+++ blog_panel/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.panel, name='panel'),
-    
+    path('', views.panel, name='PaginaInicial'),
+    # path('', views.panel_navbar, name='panel_navbar'),
+    # path('blogdashboard', views.blog_dashboard, name='blog_dashboard'),
 ]
